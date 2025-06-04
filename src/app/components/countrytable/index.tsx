e8fcd563e9500c2bb716bb8c9b68bb8f3dc225e2
Ashwink-dev <65844472+Ashwink-dev@users.noreply.github.com>
2025-06-04 00:48:04 -0400

    Added Page Title to Page.tsx and updated header text from country code to country

--- src/app/components/countrytable/index.tsx
+++ src/app/components/countrytable/index.tsx
@@ -16,7 +16,7 @@ const CountryTable: React.FC<Props> = ({ data, sortFunc }) => {
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                            Country Code
+                            Country
                         </th>
                         <th scope="col" className="px-6 py-3" onClick={() => {
                                     sortFunc('gold')
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>Olympic Results 2025</h1>
        <MainApp />
       </div>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
